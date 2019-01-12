530a0f7bf437cdcb6efd1d4f2df3cd067c95201d
altan <korukaltan@gmail.com>
2019-01-13 01:32:49 +0300

    Fix build
    Change-Id: I76df6b4a9b408324e87f3b40fcc20e17eb9dc875

--- src/org/lineageos/updater/misc/Utils.java
+++ src/org/lineageos/updater/misc/Utils.java
@@ -161,7 +161,7 @@ public class Utils {
         }
 
         return serverUrl.replace("{device}", device)
-                .replace("{type}", type)
+        .replace("{type}", type);
     }
 
     public static String getUpgradeBlockedURL(Context context) {
